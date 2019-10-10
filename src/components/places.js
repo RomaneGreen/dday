dc186998c3e088f614897ccb910c8498993dad25
Romane G <romane71193@gmail.com>
2019-10-09 20:21:55 -0400

    spacing

--- src/components/places.js
+++ src/components/places.js
@@ -8,17 +8,9 @@ export default class Googleplaces extends Component {
         }
     }
 
-    componentDidMount() {
-<<<<<<< HEAD
-        let urlOne = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.663918,-73.8820044&radius=1500&type=restaurant&keyword=drinks&key=AIzaSyCGjREc2FZhlFgX2iCtdTluvQo4Mu9Tln4/';
-        let urlTwo = 'https://randomuser.me/api/';
 
-    fetch(urlOne)
-           .then(res => res.json())
-           .then(data => this.setState({ places: data }))
-                
-           
-=======
+    
+    componentDidMount() {
     fetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.663918,-73.8820044&radius=1500&type=restaurant&keyword=drinks&key=",{
         
             method: 'GET',
@@ -34,7 +26,6 @@ export default class Googleplaces extends Component {
             this.setState({ places: result })
             console.log("places", this.state.places)
         })
->>>>>>> 0c2d6398bbcccf91f43b1a8741e0f415d8082e9e
     
     }
 
